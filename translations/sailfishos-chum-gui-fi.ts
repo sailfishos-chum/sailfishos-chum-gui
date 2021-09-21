87771f81d353887dc2c4e7bfe0a1221a13adcaab
Matti Viljanen <matti.viljanen@kapsi.fi>
2021-09-21 19:24:29 +0300

    Create Finnish translation

---
+++ translations/sailfishos-chum-gui-fi.ts
@@ -0,0 +1,47 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fi" sourcelanguage="en">
+<context>
+    <name></name>
+    <message id="chum-available-packages">
+        <location filename="../qml/pages/MainPage.qml" line="20"/>
+        <source>Available packages</source>
+        <translation>Saatavilla olevat paketit</translation>
+    </message>
+    <message id="chum-reload">
+        <location filename="../qml/pages/MainPage.qml" line="49"/>
+        <source>Reload</source>
+        <translation>Virkistä</translation>
+    </message>
+    <message id="chum-install">
+        <location filename="../qml/pages/PackagePage.qml" line="25"/>
+        <source>Install</source>
+        <translation>Asenna</translation>
+    </message>
+    <message id="chum-pkg-installed-version">
+        <location filename="../qml/pages/PackagePage.qml" line="66"/>
+        <source>Installed version</source>
+        <translation>Asennettu versio</translation>
+    </message>
+    <message id="chum-pkg-available-version">
+        <location filename="../qml/pages/PackagePage.qml" line="73"/>
+        <source>Available version</source>
+        <translation>Saatavilla oleva versio</translation>
+    </message>
+    <message id="chum-pkg-download-size">
+        <location filename="../qml/pages/PackagePage.qml" line="78"/>
+        <source>Download size</source>
+        <translation>Latauksen koko</translation>
+    </message>
+    <message id="chum-pkg-license">
+        <location filename="../qml/pages/PackagePage.qml" line="84"/>
+        <source>License</source>
+        <translation>Lisenssi</translation>
+    </message>
+    <message id="chum-pkg-link">
+        <location filename="../qml/pages/PackagePage.qml" line="94"/>
+        <source>Link</source>
+        <translation>Linkki</translation>
+    </message>
+</context>
+</TS>
